9899f1ff320885e447cf492cae773697ae95f6fd
VladanR <x3m.rocker.035@gmail.com>
2020-05-26 21:44:53 +0200

    Create UI and add take photo feature

--- SimpleCameraApp/ViewController.m
+++ SimpleCameraApp/ViewController.m
@@ -20,4 +20,36 @@
 }
 
 
+- (IBAction)savePhoto:(id)sender {
+}
+
+- (IBAction)chosePhoto:(id)sender {
+    
+    
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    
+    photo = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    [self.imageView setImage:photo];
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+
+}
+
+- (IBAction)takePhoto:(id)sender {
+    
+    photoPicker = [[UIImagePickerController alloc] init];
+    photoPicker.delegate = self;
+    [photoPicker setSourceType:UIImagePickerControllerSourceTypeCamera];
+    [self presentViewController:photoPicker animated:YES completion:NULL];
+    
+}
+
 @end
