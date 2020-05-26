811458df363101b17e2cc74e606adb80eee4b4b8
VladanR <x3m.rocker.035@gmail.com>
2020-05-26 21:27:30 +0200

    first commit

---
+++ SimpleCameraApp/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SimpleCameraApp
+//
+//  Created by Vladan Randjelovic on 26/05/2020.
+//  Copyright © 2020 Vladan Randjelovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ SimpleCameraApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SimpleCameraApp
+//
+//  Created by Vladan Randjelovic on 26/05/2020.
+//  Copyright © 2020 Vladan Randjelovic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
